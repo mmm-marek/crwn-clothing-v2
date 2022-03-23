45c909fe2749da83f3316a2debfc25a4f6cb4493
Marek Matušica <83821425+mmm-marek@users.noreply.github.com>
2022-03-23 20:00:26 +0100

    Directory component added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-import CategoryItem from "./components/category-item/category-item.component";
+import Directory from "./components/directory/directory.component";
 
-import "./categories.styles.scss";
 import categories from "./categories.json";
 
 const App = () => {
-    return (
-        <div className="categories-container">
-            {categories.map((category) => (
-                <CategoryItem key={category.id} category={category} />
-            ))}
-        </div>
-    );
+    return <Directory categories={categories} />;
 };
 
 export default App;
