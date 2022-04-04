30bf07d9ed9653f1f41fb42d3986de552904d51c
Marek Matušica <83821425+mmm-marek@users.noreply.github.com>
2022-04-04 20:24:59 +0200

    cart icon with toggle effect added, card-item component added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { UserProvider } from "./context/user.context";
+import { ProductsProvider } from "./context/products.context";
+import { CartProvider } from "./context/cart.context";
 import "./index.scss";
 import reportWebVitals from "./reportWebVitals";
 
@@ -10,7 +12,11 @@ ReactDOM.render(
     <React.StrictMode>
         <BrowserRouter>
             <UserProvider>
-                <App />
+                <ProductsProvider>
+                    <CartProvider>
+                        <App />
+                    </CartProvider>
+                </ProductsProvider>
             </UserProvider>
         </BrowserRouter>
     </React.StrictMode>,
